ed7e8ca4b93a532afcb7331e33078d4d220d18fd
Propar2750 <82144119+Propar2750@users.noreply.github.com>
2023-08-14 08:09:44 +0530

    Create All_aisn.py
    All the practicals of aisn solved

---
+++ All_aisn.py
@@ -0,0 +1,6 @@
+# Question 1 : WAP to calculate the area and circumference of a circle if radius is given
+
+
+def area_circumference_circle():
+    radius = int(input("Please enter the radius: "))
+    print(f'Area is {3.14*radius*radius}, circumference is {2*3.14*radius}')
