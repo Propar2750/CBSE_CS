e363e89accb17d4734b6b43848fd6c510cc1f610
Propar2750 <82144119+Propar2750@users.noreply.github.com>
2023-08-21 08:28:28 +0530

    Update All_aisn_term_2.py
    Question 2

--- All_aisn_term_2.py
+++ All_aisn_term_2.py
@@ -8,3 +8,48 @@ elif string_2[string_2.find(string_1)] == string_2[len(string_1)]:
     print(f"{string_2} is a prefix of {string_2}")
 else: 
     print('Nothing')
+
+
+# Question 2 : WAP to read a string and
+# i ) Print frequency of all characters
+# ii) Word of highest length
+# iii) The string in Title Case
+# iv) Read the full name and print only the first letters and the last name
+def question_2():
+    
+    
+    def i():
+        string = input("Enter the string: ")
+        output = {}
+        string = list(string)
+        for i in range(len(string)):
+            try: 
+                output[string[i]] = output[string[i]] + 1
+            except KeyError:
+                output[string[i]] = 1
+        print(output)
+        
+        
+    def ii():
+        string = input("Enter the string: ").split(" ")
+        longest_word = ""
+        for i in range(len(string)):
+            if len(string[i]) > len(longest_word):
+                longest_word = string[i]
+        print(longest_word)
+        
+        
+    def iii():
+        string = input("Enter the string: ").split(" ")
+        for i in range(len(string)):
+            if string[i].istitle():
+                print(string[i])
+                
+    
+    def iv():
+        string = input("Enter the string: ").split(" ")
+        for i in range(len(string)):
+            if i != len(string) - 1 :
+                print(string[i][0], end = " ")
+            else:
+                print(string[i])
